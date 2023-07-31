255b054ea92128357a315ae4112a584c5005b4d8
Taras Mikhaylov <jamahbully@gmail.com>
2023-08-01 01:25:36 +0500

    Actions

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -67,7 +67,7 @@ DATABASES = {
 	'USER': os.getenv('POSTGRES_USER', 'django'),
 	'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
 	'HOST': os.getenv('DB_HOST', ''),
-	'PORT': os.getenv('DB_PORT', 5432)
+	'PORT': os.getenv('DB_PORT', 5432),
     }
 }
 
